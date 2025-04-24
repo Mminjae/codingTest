ddaa4ac854949480a4b20cd46402ee5965c5e19f
Maeng minjae <136868351+Mminjae@users.noreply.github.com>
2025-04-24 17:21:00 +0900

    [level 2] Title: 주식가격, Time: 21.12 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] solution(int[] prices) {
+
+        int[] result = new int[prices.length];
+
+        for(int i = 0; i < prices.length; i++){
+            for (int j = i+1; j <prices.length; j++){
+                result[i] += 1;
+                if(prices[i]>prices[j]) break;
+            }
+        }
+        return result;
+    }
+}   
